f19d96d110270ddec4862e53fc82ef2b64fed411
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2026-06-29 13:22:41 -0300

    feat: add FakeTikTokChatProvider and ServiceHelper for worker control service tests

---
+++ test/LCB.UnitTest/Helpers/ServiceHelper.cs
@@ -0,0 +1,55 @@
+using System;
+using LCB.Application.Services;
+using LCB.Domain.Entities;
+using LCB.Domain.Interfaces.Repositories;
+using LCB.UnitTest.Fakes;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+
+namespace LCB.UnitTest.Helpers;
+
+public static class ServiceHelper
+{
+    public static (WorkerControlService Service, Guid UserIdA, Guid UserIdB) Create(LiveSettingsEntity settings = null)
+    {
+        var userIdA = Guid.NewGuid();
+        var userIdB = Guid.NewGuid();
+
+        if (settings is not null)
+            settings = LiveSettingsEntity.Create(
+                userIdA,
+                settings.UpdatedByUser,
+                settings.TikTokUsername,
+                settings.TwitchUsername,
+                settings.YouTubeUsername,
+                settings.ReloadTimeInSec);
+
+        var repository = new Mock<ILiveSettingsRepository>();
+        repository
+            .Setup(x => x.GetByUserIdAsync(It.IsAny<Guid>()))
+            .ReturnsAsync((Guid id) => id == userIdA ? settings : null);
+
+        var serviceProvider = new Mock<IServiceProvider>();
+        serviceProvider
+            .Setup(x => x.GetService(typeof(ILiveSettingsRepository)))
+            .Returns(repository.Object);
+
+        var scope = new Mock<IServiceScope>();
+        scope
+            .SetupGet(x => x.ServiceProvider)
+            .Returns(serviceProvider.Object);
+
+        var scopeFactory = new Mock<IServiceScopeFactory>();
+        scopeFactory
+            .Setup(x => x.CreateScope())
+            .Returns(scope.Object);
+
+        var service = new WorkerControlService(
+            scopeFactory.Object,
+            new FakeTikTokChatProvider(),
+            NullLogger<WorkerControlService>.Instance);
+
+        return (service, userIdA, userIdB);
+    }
+}
--- test/LCB.UnitTest/Workers/ChatWorkerTests.cs
+++ test/LCB.UnitTest/Workers/ChatWorkerTests.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Channels;
@@ -6,11 +5,8 @@ using System.Threading.Tasks;
 using LCB.Application.Services;
 using LCB.Application.Workers;
 using LCB.Domain.Models;
-using LCB.Domain.Models.Config;
-using LCB.Infrastructure.Providers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace LCB.UnitTest.Workers;
@@ -18,44 +14,22 @@ namespace LCB.UnitTest.Workers;
 public class ChatWorkerTests
 {
     [Fact]
-    public async Task ExecuteAsync_Returns_WhenTikTokUsernameIsMissing()
+    public async Task ExecuteAsync_Completes_WhenChannelIsCompleted()
     {
         var channel = Channel.CreateUnbounded<ChatMessageModel>();
         channel.Writer.Complete();
         var provider = new ServiceCollection().BuildServiceProvider();
 
         var worker = new ChatWorker(
-            new TikTokChatProvider(channel.Writer, NullLogger<TikTokChatProvider>.Instance),
             new ChatProcessorService(channel.Reader,
                                      provider.GetRequiredService<IServiceScopeFactory>(),
                                      NullLogger<ChatProcessorService>.Instance),
-            NullLogger<ChatWorker>.Instance,
-            new TestOptionsMonitor(new LiveConfig { Tiktok = string.Empty }));
+            NullLogger<ChatWorker>.Instance);
 
         var method = typeof(ChatWorker).GetMethod("ExecuteAsync", BindingFlags.Instance | BindingFlags.NonPublic);
         Assert.NotNull(method);
 
         var task = (Task)method!.Invoke(worker, [CancellationToken.None])!;
         await task;
-    }
-
-    private sealed class TestOptionsMonitor(LiveConfig value) : IOptionsMonitor<LiveConfig>
-    {
-        public LiveConfig CurrentValue { get; } = value;
-
-        public LiveConfig Get(string name)
-            => CurrentValue;
-
-        public IDisposable OnChange(Action<LiveConfig, string> listener)
-            => NullDisposable.Instance;
-    }
-
-    private sealed class NullDisposable : IDisposable
-    {
-        public static readonly NullDisposable Instance = new();
-
-        public void Dispose()
-        {
-        }
     }
 }
